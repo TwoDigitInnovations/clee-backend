f380bfdb89a6a1a98b7f7dba668e76c1ba9af0d8
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2026-03-20 10:15:21 +0530

    add logic

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const {
+  login,
+  register,
+  sendOTP,
+  verifyOTP,
+  changePassword,
+  myProfile,
+  getAllUser,
+  updateprofile,
+  updateStatus,
+
+} = require('@controllers/authController');
+const auth = require('@middlewares/authMiddleware');
+// const { upload } = require('@services/fileUpload');
+
+router.post('/login', login);
+router.post('/register', register);
+router.post('/updateStatus', auth('admin'), updateStatus);
+router.post('/sendOTP', sendOTP);
+router.post('/verifyOTP', verifyOTP);
+router.post('/changePassword', changePassword);
+router.get('/profile', auth('admin', 'user'), myProfile);
+router.post(
+  '/updateprofile',
+  auth('admin', 'user'),
+  // upload.single('image'),
+  updateprofile,
+);
+router.get('/getAllUser', auth('admin'), getAllUser);
+module.exports = router;
